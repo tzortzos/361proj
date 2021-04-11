0d6a903c473fc1d62388d0acb3c357801755cb3c
Josiah Hilden <jhilden13@gmail.com>
2021-04-10 23:33:08 -0500

    Added very simple Index view
    - Added view `Index` in views.py
    - Added template `index.html` in templates
    - Added url with name `index` in urls.py
    - Updated settings to reflect project

--- project/TAScheduler/views.py
+++ project/TAScheduler/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class Index(View):
+
+    def get(self, request):
+        return render(request, 'index.html')
+
+    def post(self, request):
+        return render(request, 'index.html')
--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from TAScheduler.views import Index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index.as_view(), name='index'),
 ]
