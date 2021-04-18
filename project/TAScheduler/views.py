6395b6a6185d77db5a0b0705c17c5851c3a03fbd
Josiah Hilden <jhilden13@gmail.com>
2021-04-17 22:46:11 -0500

    Example view for testing partials

--- project/TAScheduler/views.py
+++ project/TAScheduler/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 from TAScheduler.viewsupport.navbar import AdminItems
+from TAScheduler.viewsupport.errors import PageError, LoginError
 
 # Create your views here.
 class Index(View):
@@ -17,7 +18,9 @@ class Index(View):
 
 class Login(View):
     def get(self, request):
-        return render(request, 'pages/login.html')
+        return render(request, 'pages/login.html', {
+            'error': LoginError(PageError('This is a password error'), LoginError.Place.USERNAME)
+        })
 
     def post(self, request):
         print('User Tried to log in')
