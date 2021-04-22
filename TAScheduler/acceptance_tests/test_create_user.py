abaf73a1f434ab136bff1fc339e8439aefb87231
Josiah Hilden <jhilden13@gmail.com>
2021-04-22 13:07:22 -0500

    Finish acceptance tests

--- TAScheduler/acceptance_tests/test_create_user.py
+++ TAScheduler/acceptance_tests/test_create_user.py
@@ -1,7 +1,10 @@
+from django.shortcuts import reverse
 from django.test import TestCase, Client
 from django.http import HttpRequest, HttpResponse
+from django.db.models import ObjectDoesNotExist
 
 from TAScheduler.models import User, UserType
+from TAScheduler.viewsupport.errors import PageError, UserEditError
 
 
 class TestCreateUserView(TestCase):
@@ -12,13 +15,31 @@ class TestCreateUserView(TestCase):
         # session to have one for most tests
 
         self.admin_username = 'josiahth'
-        self.admin = User.objects.create(univ_id=self.admin_username, password='a-very-good-password', type=UserType.ADMIN)
+        self.admin = User.objects.create(univ_id=self.admin_username,
+                                         password='a-very-good-password',
+                                         type=UserType.ADMIN,
+                                         tmp_password=False)
+
         self.client.session['user_id'] = self.admin.user_id
 
+        self.prof_username = 'jrock'
+        self.professor = User.objects.create(univ_id=self.prof_username,
+                                             password='a-very-goog-password',
+                                             type=UserType.PROF,
+                                             tmp_password=False)
+
+    def get_error(self, resp: HttpResponse) -> UserEditError:
+        resp_context = resp.context
+
+        self.assertIsNotNone(resp_context['error'], 'Did not return error in context on empty password')
+        self.assertEqual(type(resp_context['error']), UserEditError, 'Did not return correctly typed error')
+
+        return resp_context['error']
+
     def test_rejects_empty_password(self):
-        resp = self.client.post(reversed('create-user'), {
-            'univ_id': 'nathanl',
-            'password': '',
+        resp = self.client.post(reverse('create-user'), {
+            'univ_id': 'nleverence',
+            # 'new_password': '',
             'user_type': 'T',
 
         }, follow=True)
@@ -26,25 +47,129 @@ class TestCreateUserView(TestCase):
         self.assertIsNotNone(resp, 'Did not return a response')
         self.assertEqual(len(resp.redirect_chain), 0, 'Should not redirect on failed insertion')
 
-        resp_context = resp.context
+        ret_error = self.get_error(resp)
 
-        self.assertIsNotNone(resp_context['error'], 'Did not return error in context on empty password')
-        self.assertEqual(type(resp_context['error']))
+        self.assertTrue(ret_error.place() is UserEditError.Place.PASSWORD, 'Did not recognize that password was empty')
+        self.assertEqual(ret_error.error().body(), 'You must provide a password to create a user')
 
     def test_rejects_short_password(self):
-        pass
+        resp = self.client.post(reverse('create-user'), {
+            'univ_id': 'nleverence',
+            'new_password': '1234567',  # Short password
+            'user_type': 'T',
+
+        }, follow=True)
+
+        self.assertIsNotNone(resp, 'Did not return a response')
+        self.assertEqual(len(resp.redirect_chain), 0, 'Should not redirect on failed insertion')
+
+        ret_error = self.get_error(resp)  # Asserts that an error exists as well
+
+        self.assertTrue(ret_error.place() is UserEditError.Place.PASSWORD, 'Did not recognize that password was empty')
+        self.assertEqual(ret_error.error().body(), 'Password must be at least 8 characters in length')
 
     def test_rejects_empty_username(self):
-        pass
+        resp = self.client.post(reverse('create-user'), {
+            # 'univ_id': '',
+            'new_password': 'a-very-good-password',  # Short password
+            'user_type': 'T',
+
+        }, follow=True)
+
+        self.assertIsNotNone(resp, 'Did not return a response')
+        self.assertEqual(len(resp.redirect_chain), 0, 'Should not redirect on failed insertion')
+
+        ret_error = self.get_error(resp)  # Asserts that an error exists as well
+
+        self.assertTrue(ret_error.place() is UserEditError.Place.USERNAME, 'Did not recognize missing username')
+        self.assertEqual(ret_error.error().body(), 'You must provide a university id')
 
     def test_rejects_long_username(self):
-        pass
+        resp = self.client.post(reverse('create-user'), {
+            'univ_id': 'a-very-long-username-that-would-never-fit-in-the-database',
+            'new_password': 'a-very-good-password',
+            'user_type': 'T',
 
-    def test_rejects_username_with_special_characters(self):
-        pass
+        }, follow=True)
+
+        self.assertIsNotNone(resp, 'Did not return a response')
+        self.assertEqual(len(resp.redirect_chain), 0, 'Should not redirect on failed insertion')
+
+        ret_error = self.get_error(resp)  # Asserts that an error exists as well
+
+        self.assertTrue(ret_error.place() is UserEditError.Place.USERNAME, 'Did not recognize missing username')
+        self.assertEqual(ret_error.error().body(), 'The username may not be longer than 20 characters')
+
+    def test_rejects_username_with_at_sign(self):
+        resp = self.client.post(reverse('create-user'), {
+            'univ_id': 'areasonableusername@uwm.edu',
+            'new_password': 'a-very-good-password',
+            'user_type': 'T',
+
+        }, follow=True)
+
+        self.assertIsNotNone(resp, 'Did not return a response')
+        self.assertEqual(len(resp.redirect_chain), 0, 'Should not redirect on failed insertion')
+
+        ret_error = self.get_error(resp)  # Asserts that an error exists as well
+
+        self.assertTrue(ret_error.place() is UserEditError.Place.USERNAME, 'Did not recognize missing username')
+        self.assertEqual(ret_error.error().body(), 'You only need to put in the first part of a university email')
+
+    def test_rejects_username_with_spaces(self):
+        resp = self.client.post(reverse('create-user'), {
+            'univ_id': 'areasonableusername uwm edu',
+            'new_password': 'a-very-good-password',
+            'user_type': 'T',
+
+        }, follow=True)
+
+        self.assertIsNotNone(resp, 'Did not return a response')
+        self.assertEqual(len(resp.redirect_chain), 0, 'Should not redirect on failed insertion')
+
+        ret_error = self.get_error(resp)  # Asserts that an error exists as well
+
+        self.assertTrue(ret_error.place() is UserEditError.Place.USERNAME, 'Did not recognize missing username')
+        self.assertEqual(ret_error.error().body(), 'A username may not have spaces')
 
     def test_rejects_non_admin(self):
-        pass
+        self.client.session['user_id'] = self.professor.user_id
+        resp = self.client.post(reverse('create-user'), {
+            'univ_id': 'nleverence',
+            'new_password': 'a-very-good-password',
+            'user_type': 'T',
+
+        }, follow=True)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            User.objects.get(univ_id='nleverence')
+
+        redirects = resp.redirect_chain
+
+        self.assertEqual(len(redirects), 1, 'Redirected the wrong number of times')
+        self.assertEqual(
+            redirects[0][0],
+            reverse('user-directory'),
+            'Did not redirect user to user directory in the case that they were not allowed to create users'
+        )
 
     def test_rejects_missing_session(self):
-        pass
+        del self.client.session['user_id']
+        resp = self.client.post(reverse('create-user'), {
+            'univ_id': 'nleverence',
+            'new_password': 'a-very-good-password',
+            'user_type': 'T',
+
+        }, follow=True)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            User.objects.get(univ_id='nleverence')
+
+        redirects = resp.redirect_chain
+
+        self.assertEqual(len(redirects), 1, 'Redirected the wrong number of times')
+        self.assertEqual(
+            redirects[0][0],
+            reverse('index'),
+            'Did not redirect user to user directory in the case that they were not allowed to create users'
+        )
