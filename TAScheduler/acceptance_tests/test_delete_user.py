101575d203b77f1a794d226a1cf248cd88e15f29
Josiah Hilden <jhilden13@gmail.com>
2021-04-22 14:02:20 -0500

    Finish delete user acceptance tests

--- TAScheduler/acceptance_tests/test_delete_user.py
+++ TAScheduler/acceptance_tests/test_delete_user.py
@@ -6,8 +6,8 @@ from django.db.models import ObjectDoesNotExist
 from TAScheduler.models import User, UserType
 from TAScheduler.viewsupport.message import Message
 
-class TestDeleteUser(TestCase):
 
+class TestDeleteUser(TestCase):
     def setUp(self):
         self.client = Client()
 
@@ -33,32 +33,69 @@ class TestDeleteUser(TestCase):
 
         self.client.session['user_id'] = self.prof.user_id
 
+    def get_first_message(self, resp) -> Message:
+        self.assertIsNotNone(resp.context['messages'], 'Did not send success message to user')
+        self.assertTrue(len(resp.context['messages']) == 1, 'Did not send any success messages to user')
+
+        return resp.context['messages'][0]
+
+
     def test_admin_can_delete_existing(self):
         resp = self.client.post(self.valid_delete_url, {}, follow=True)
 
         self.assertIsNotNone(resp, 'Post did not return value')
+        self.assertRedirects(resp, reverse('user-directory'), 'Did not redirect after deletion')
 
-        redirect_chain = resp.redirect_chain
-
-        self.assertEqual(len(redirect_chain), 1, 'Did not redirect the correct number of times')
-        self.assertEqual(redirect_chain[0][0], reverse('user-directory'), 'Did not redirect to user directory on delete')
-
-        self.assertIsNotNone(resp.context['messages'], 'Did not send success message to user')
-        self.assertTrue(len(resp.context['message']) >= 1, 'Did not send any success messages to user')
-
-        message: Message = resp.context['message']
+        message: Message = self.get_first_message(resp)
 
         self.assertTrue(message.type() is Message.Type.REGULAR, 'Did not send correct message type')
         self.assertEqual(message.message(), f'Successfully deleted user {self.prof_username}', 'Did not return correct message')
 
     def test_delete_nonexistent_redirects_with_error(self):
-        pass
+        resp = self.client.post(self.invalid_delete_url, {}, follow=True)
+
+        self.assertIsNotNone(resp, 'Post did not return value')
+        self.assertRedirects(resp, reverse('user-directory'), 'Did not redirect after deletion')
+
+        message: Message = self.get_first_message(resp)
+
+        self.assertTrue(message.type() is Message.Type.ERROR, 'Did not send correct message type')
+        self.assertEqual(message.message(), f'User with id {340000} does not exist', 'Did not return correct message')
 
     def test_prof_cannot_delete_anyone(self):
-        pass
+        self.client.session['user-id'] = self.prof.user_id
+
+        resp_post = self.client.post(reverse('user-delete', self.admin.user_id), {}, follow=True)
+        resp_get = self.client.get(reverse('user-delete', self.admin.user_id), {}, follow=True)
+
+        self.assertIsNotNone(resp_post, 'Post did not return value')
+        self.assertIsNotNone(resp_get, 'Get did not return value')
+
+        self.assertRedirects(resp_post, reverse('user-directory'))
+        self.assertRedirects(resp_get, reverse('user-directory'))
+
+        message_post = self.get_first_message(resp_post)
+        message_get = self.get_first_message(resp_get)
+
+        self.assertTrue(message_post.type() is Message.Type.ERROR, 'Did not send correct message type')
+        self.assertEqual(message_post.message(), f'You do not have permission to delete users', 'Did not return correct message')
+
+        self.assertTrue(message_get.type() is Message.Type.ERROR, 'Did not send correct message type')
+        self.assertEqual(message_get.message(), f'You do not have permission to delete users', 'Did not return correct message')
+
+    def test_no_session(self):
+        del self.client.session['user-id']
+        resp_post = self.client.post(self.valid_delete_url, {}, follow=True)
+        resp_get = self.client.get(self.valid_delete_url, {}, follow=True)
+
+        self.assertIsNotNone(resp_post, 'Post did not return value')
+        self.assertIsNotNone(resp_get, 'Get did not return value')
+
+        self.assertRedirects(resp_post, reverse('index'))
+        self.assertRedirects(resp_get, reverse('index'))
 
     def test_delete_removes(self):
-        pass
+        self.client.post(self.valid_delete_url, {}, follow=True)
 
-    def test_delete_redirects(self):
-        pass
+        with self.assertRaises(ObjectDoesNotExist, msg='Did not remove from database'):
+            User.objects.get(univ_id=self.prof_username)
