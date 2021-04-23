12f20d87b9f6155e58e037c63ad9c22af93bd940
NRL <lnahnan@uwm.edu>
2021-04-22 21:01:19 -0500

    Implementation of create user View

--- TAScheduler/views/users/create.py
+++ TAScheduler/views/users/create.py
@@ -7,27 +7,95 @@ import uuid
 from TAScheduler.ClassDesign.LoginUtility import LoginUtility
 from TAScheduler.ClassDesign.UserAPI import UserAPI, UserType
 from TAScheduler.viewsupport.navbar import AdminItems
+from TAScheduler.viewsupport.errors import PageError, UserEditError
 
 class UserCreate(View):
 
     def get(self, request: HttpRequest) -> Union[HttpResponse, HttpResponseRedirect]:
-        try:
-            user_id= request.session['user_id']
-        except KeyError:
-            return redirect(reverse('login'))
+        maybe_user = LoginUtility.get_user_and_validate_by_user_id(
+            request.session,
+            [UserType.ADMIN],
+        )
+        if type(maybe_user) is HttpResponseRedirect:
+            return maybe_user
 
-        user = LoginUtility.get_user_and_validate_by_user_id(user_id)
-
-        # check user is admin
-        if UserAPI.check_user_type(user) is not UserType.ADMIN:
-            return redirect('index')
-
-        tmp_pass = str(uuid.uuid4())[:8]
+        tmp_pass = LoginUtility.generate_tmp_password()
 
         return render(request, 'pages/create_user.html', {
-            'self': user,
+            'self': maybe_user,
             'navbar_items': AdminItems.items_iterable(),
             'new_user_pass': tmp_pass
         })
+
+    def post(self, request: HttpRequest):
+        maybe_user = LoginUtility.get_user_and_validate_by_user_id(
+            request.session,
+            [UserType.ADMIN],
+        )
+        if type(maybe_user) is HttpResponseRedirect:
+            return maybe_user
+
+        try:
+            new_pass = str(request.POST['new_password'])
+        except:
+            new_pass = None
+
+        if new_pass is None or len(new_pass) < 8:
+            return render(request, 'pages/create_user.html', {
+                'self': maybe_user,
+                'error': UserEditError('Password must be at least 8 characters in length', UserEditError.Place.PASSWORD),
+                'new_user_pass': LoginUtility.generate_tmp_password(),
+            })
+        try:
+            univ_id = str(request.POST['univ_id'])
+        except:
+            univ_id = None
+
+        if univ_id is None or len(univ_id) == 0:
+            return render(request, 'pages/create_user.html', {
+                'self': maybe_user,
+                'error': UserEditError('You must provide a university id', UserEditError.Place.USERNAME),
+                'new_user_pass': new_pass,
+            })
+        # TODO: Check univ id for special characters.
+
+        try:
+            user_type = str(request.POST['user_type'])
+        except:
+            user_type = None
+
+        if user_type is None or user_type not in ['A', 'P', 'T']:
+            return render(request, 'pages/create_user.html', {
+                'self': maybe_user,
+                'error': UserEditError('You must provide a user type', UserEditError.Place.TYPE),
+                'new_user_pass': new_pass,
+            })
+
+        try:
+            l_name = str(request.POST['l_name'])
+        except TypeError:
+            l_name = None
+
+        try:
+            f_name = request.POST['f_name']
+        except TypeError:
+            f_name = None
+
+        try:
+            phone  = request.POST['phone']
+
+        except TypeError:
+            phone = None
+
+        user_id = UserAPI.create_user(user_type, univ_id, new_pass)
+        UserAPI.update_user(UserAPI.get_user_by_user_id(user_id), l_name, f_name, phone)
+
+        return redirect(reverse('users-directory'))
+
+
+
+
+
+
 
 
