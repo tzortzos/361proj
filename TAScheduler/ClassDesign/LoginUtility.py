891be15225a52f7a8230932635b1e0a59d3a62e1
Josiah Hilden <jhilden13@gmail.com>
2021-04-22 22:55:50 -0500

    Adds Message queue functionality to LoginUtility user validation function

--- TAScheduler/ClassDesign/LoginUtility.py
+++ TAScheduler/ClassDesign/LoginUtility.py
@@ -1,10 +1,11 @@
 import uuid
-from typing import Union
+from typing import Union, Optional
 from django.http import HttpResponseRedirect
-from django.shortcuts import redirect,reverse
+from django.shortcuts import redirect, reverse
 
 from TAScheduler.ClassDesign.UserAPI import UserAPI, UserType
 from TAScheduler.models import User
+from TAScheduler.viewsupport.message import Message, MessageQueue
 
 
 class LoginUtility:
@@ -23,11 +24,13 @@ class LoginUtility:
     def get_user_and_validate_by_user_id(
             session,
             types: list[UserType] = [],
-            redirect_to : HttpResponseRedirect = redirect(reverse('index'))
-    ) -> Union[User,HttpResponseRedirect]:
+            redirect_to : HttpResponseRedirect = redirect(reverse('index')),
+            redirect_message: Optional[Message] = None,
+    ) -> Union[User, HttpResponseRedirect]:
         """
         Get a user from a request context and validate the user's type
-        To ensure that the user has permission to look at this page at this time
+        To ensure that the user has permission to look at this page at this time.
+        If the user is redirected for being the incorrect type then `redirect_message` is added to their message queue.
         """
         try:
             user_id = session['user_id']
@@ -35,19 +38,26 @@ class LoginUtility:
             user_id = None
 
         if user_id is None or type(user_id) is not int:
+            MessageQueue.push(session, Message('You must log into the application before you can view that page'))
             return redirect(reverse('login'))
 
         user = UserAPI.get_user_by_user_id(user_id)
 
         if user is None:
+            MessageQueue.push(session, Message('You must log into the application before you can view that page'))
             return redirect(reverse('login'))
 
         if user.tmp_password:
+            MessageQueue.push(session, Message('You must change your password before accessing the application'))
             return redirect(reverse('user-edit', args=(user_id,)))
 
         user_type = UserAPI.check_user_type(user)
 
-        if len(types)>0 and user_type not in types:
+        if len(types) > 0 and user_type not in types:
+
+            if redirect_message is not None:
+                MessageQueue.push(session, redirect_message)
+
             return redirect_to
 
         return user
