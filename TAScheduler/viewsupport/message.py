a0312df64f9b293f63d6e31afe9a7613f8300258
Josiah Hilden <jhilden13@gmail.com>
2021-04-22 22:43:11 -0500

    Adds message queue concept to sessions so that messages can be sent across pages discreetly

--- TAScheduler/viewsupport/message.py
+++ TAScheduler/viewsupport/message.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+from typing import Iterable, List, Union
 
 class Message:
     """
@@ -25,3 +26,54 @@ class Message:
         if type(other) is not Message:
             return False
         return self._ty.value == other._ty.value and self._message == other._message
+
+
+class MethodQueue:
+    """
+    Methods for sending messages to the user across application pages.
+    Should be drained into view on each page load.
+    """
+
+    @staticmethod
+    def drain(session) -> Iterable[Message]:
+        """Take all messages from the message queue"""
+        try:
+            messages: List[Message] = session['messages']
+
+            # Clear message queue
+            del session['messages']
+
+            return iter(messages)
+        except KeyError:
+            return iter([])
+
+    @staticmethod
+    def drain_n(session, n: int) -> Iterable[Message]:
+        """Take up to n messages from the message queue"""
+        try:
+            messages: List[Message] = session['messages']
+            m_len = len(messages)
+
+            # Take n from message queue, or remainder if there are fewer than n left
+            if m_len < n:
+                del session['messages']
+                return iter(messages)
+            else:
+                session['messages'] = messages[n:]
+                return iter(messages[:n])
+
+        except KeyError:
+            return iter([])
+
+    @staticmethod
+    def push(session, messages: Union[List[Message], Message]):
+        """Add a message to the message queue for the next page render"""
+        try:
+            current: List[Message] = session['messages']
+        except KeyError:
+            current: List[Message] = []
+
+        if type(messages) is Message:
+            session['messages'] = current + [messages]
+        else:
+            session['messages'] = current + messages
