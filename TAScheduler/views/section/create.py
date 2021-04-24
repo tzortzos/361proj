7b27b5b7749827ed97c81287d30092ff1c30a8fd
Josiah Hilden <jhilden13@gmail.com>
2021-04-24 18:03:05 -0500

    sections-create get implementation and part of post

--- TAScheduler/views/section/create.py
+++ TAScheduler/views/section/create.py
@@ -4,13 +4,48 @@ from django.shortcuts import render, redirect, reverse
 from typing import List, Union
 
 from TAScheduler.ClassDesign.LoginUtility import LoginUtility
+from TAScheduler.ClassDesign.UserAPI import User, UserType, UserAPI
 from TAScheduler.viewsupport.message import Message, MessageQueue
 from TAScheduler.viewsupport.navbar import AdminItems
+from TAScheduler.models import User, UserType, Course
 
 
 class SectionsCreate(View):
     def get(self, request: HttpRequest) -> Union[HttpResponse, HttpResponseRedirect]:
-        pass
+        user = LoginUtility.get_user_and_validate_by_user_id(
+            request.session,
+            [UserType.ADMIN],
+            reverse('sections-directory'),
+            Message('You do not have permission to create Course Sections', Message.Type.ERROR),
+        )
+
+        if type(user) is HttpResponseRedirect:
+            return user
+
+        return render(request, 'pages/sections/edit_create.html', {
+            'self': user,
+            'navbar_items': AdminItems.items_iterable(),
+
+            'courses': Course.objects.all(),
+            'professors': User.objects.filter(type=UserType.PROF),
+            'tas': User.objects.filter(type=UserType.TA),
+        })
 
     def post(self, request: HttpRequest) -> Union[HttpResponse, HttpResponseRedirect]:
-        pass
+        user = LoginUtility.get_user_and_validate_by_user_id(
+            request.session,
+            [UserType.ADMIN],
+            reverse('sections-directory'),
+            Message('You do not have permission to create Course Sections', Message.Type.ERROR),
+        )
+
+        if type(user) is HttpResponseRedirect:
+            return user
+
+        # TODO create a helper function for views getting a set of variables out of a context
+        section_code = request.POST.get('section_code')
+        course_id = request.POST.get('course_id', -1)
+        lecture_days = request.POST.getlist('lecture_days')
+        lecture_time = request.POST.get('lecture_time')
+        instructor_id = request.POST.get('instructor_id')
+        ta_ids = request.POST.getlist('ta_ids')
