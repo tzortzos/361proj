56dd750eade2c63111148ec19fa2e4a39f2bfd38
Josiah Hilden <jhilden13@gmail.com>
2021-04-23 21:45:59 -0500

    Add user information page and reusable card for rendering single users

--- TAScheduler/views/users/view.py
+++ TAScheduler/views/users/view.py
@@ -1,15 +1,31 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 from TAScheduler.viewsupport.message import MessageQueue, Message
 from TAScheduler.viewsupport.navbar import AdminItems
+
+from TAScheduler.ClassDesign.LoginUtility import LoginUtility
+from TAScheduler.ClassDesign.UserAPI import UserAPI
 
 
 class UserView(View):
 
     def get(self, request: HttpRequest, user_id: int):
-        return render(request, 'pages/example.html', context={
-            'navbar_items': AdminItems.HOME.items_iterable_except(),
+        user = LoginUtility.get_user_and_validate_by_user_id(request.session)
+
+        if type(user) is HttpResponseRedirect:
+            return user
+
+        to_display = UserAPI.get_user_by_user_id(user_id)
+
+        if to_display is None:
+            MessageQueue.push(request.session, Message(f'No user with id {user_id} exists', Message.Type.ERROR))
+            return redirect(reverse('index'))
+
+        return render(request, 'pages/user_information.html', context={
+            'navbar_items': AdminItems.items_iterable(),
             'messages': MessageQueue.drain(request.session),
+            'self': user,
+            'user': to_display,
         })
