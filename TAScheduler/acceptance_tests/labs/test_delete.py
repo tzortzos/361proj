fb5886f3628e3d835136533bbe433f85e3da9549
Josiah Hilden <jhilden13@gmail.com>
2021-04-25 23:26:08 -0500

    Adds acceptance test for delete

--- TAScheduler/acceptance_tests/labs/test_delete.py
+++ TAScheduler/acceptance_tests/labs/test_delete.py
@@ -1,6 +1,13 @@
+from django.test import Client
+from django.shortcuts import reverse
+
+from typing import List
+
 from TAScheduler.acceptance_tests.acceptance_base import TASAcceptanceTestCase
 from TAScheduler.viewsupport.errors import LabError
-from django.test import Client
+from TAScheduler.viewsupport.message import Message, MessageQueue
+
+from TAScheduler.models import User, UserType, Course, CourseSection, LabSection
 
 
 class LabsDelete(TASAcceptanceTestCase[LabError]):
@@ -9,11 +16,90 @@ class LabsDelete(TASAcceptanceTestCase[LabError]):
         self.client = Client()
         self.session = self.client.session
 
+        # Add users
+        self.admin_user = User.objects.create(
+            univ_id='josiahth',
+            password='password',
+            type=UserType.ADMIN,
+            tmp_password=False
+        )
+
+        # Add prerequisite objects
+        self.course = Course.objects.create(
+            course_code='361',
+            course_name='Software Engineering',
+            admin_id=self.admin_user,
+        )
+
+        self.ta_user = User.objects.create(
+            univ_id='nleverence',
+            password='password',
+            type=UserType.TA,
+            tmp_password=False
+        )
+
+        self.prof_user = User.objects.create(
+            univ_id='rock',
+            password='password',
+            type=UserType.PROF,
+            tmp_password=False
+        )
+
+        self.section = CourseSection.objects.create(
+            course_section_code='201',
+            course_id=self.course,
+        )
+
+        self.lab_partial = LabSection.objects.create(
+            lab_section_code='901',
+            course_section_id=self.section,
+        )
+
+        self.lab_full = LabSection.objects.create(
+            lab_section_code='901',
+            course_section_id=self.section,
+            lab_days='MWF',
+            lab_time='2-4',
+            ta_id=self.ta_user,
+        )
+
+        # Set current user
+        self.session['user_id'] = self.admin_user.user_id
+        self.session.save()
+
     def test_redirects_professor(self):
-        pass
+        self.session['user_id'] = self.prof_user.user_id
+        self.session.save()
+
+        resp = self.client.post(reverse('labs-delete', args=[self.lab_full.course_section_id]))
+
+        self.assertContainsMessage(resp, Message(
+            'You do not have permission to delete lab sections',
+            Message.Type.ERROR,
+        ))
+
+        self.assertRedirects(resp, reverse('labs-directory'))
 
     def test_redirects_ta(self):
-        pass
+        self.session['user_id'] = self.ta_user.user_id
+        self.session.save()
 
-    def test_context_contains_lab(self):
-        pass
+        resp = self.client.post(reverse('labs-delete', args=[self.lab_full.course_section_id]))
+
+        self.assertContainsMessage(resp, Message(
+            'You do not have permission to delete lab sections',
+            Message.Type.ERROR,
+        ))
+
+        self.assertRedirects(resp, reverse('labs-directory'))
+
+    def test_removes_database(self):
+        resp = self.client.post(reverse('labs-delete', args=[self.lab_full.course_section_id]))
+
+        labs: List[LabSection] = list(LabSection.objects.all())
+
+        self.assertEqual(1, len(labs), 'Did not remove lab section')
+
+        self.assertContainsMessage(resp, Message(
+            'Successfully deleted lab section'
+        ))
