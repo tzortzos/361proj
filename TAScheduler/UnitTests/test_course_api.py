735e9c15e951d7c5708dd25300202e6086b772dd
NRL <lnahnan@uwm.edu>
2021-04-25 20:15:35 -0500

    Additions to CourseAPI unit tests

--- TAScheduler/UnitTests/test_course_api.py
+++ TAScheduler/UnitTests/test_course_api.py
@@ -1,8 +1,12 @@
+
+
 from TAScheduler.ClassDesign.CourseAPI import CourseAPI
 from TAScheduler.models import UserType,User,Course
 from django.test import TestCase
 from django.core.exceptions import ObjectDoesNotExist
+from typing import List
 import uuid
+
 
 
 class TestCourse(TestCase):
@@ -10,26 +14,45 @@ class TestCourse(TestCase):
     def setUp(self):
         self.course_code1 = str(uuid.uuid4())[:3]
         self.course_code2 = str(uuid.uuid4())[:3]
+        self.course_code3 = str(uuid.uuid4())[:3]
         self.course_name1 = 'TestCourse'
+        self.course_name2 = 'TestCourse2'
         self.user1 = User.objects.create(type=UserType.ADMIN, univ_id='bsmith', password='password123')
         self.course1 = Course.objects.create(course_code=self.course_code1,course_name=self.course_name1,admin_id=self.user1)
+        self.course2 = Course.objects.create(course_code=self.course_code2,course_name=self.course_name2,admin_id=self.user1)
 
     def test_create_course(self):
         new_course_id = CourseAPI.create_course(self.course_code1, self.course_name1, self.user1)
         self.assertTrue(new_course_id > 0, msg='Expecting the course Id to be a positive integer if added.')
+
+    def test_rejects_empty_course_code(self):
+        with self.assertRaises(TypeError, msg='Course code should not be blank.'):
+            CourseAPI.create_course('', self.course_name1, self.user1)
+
+    def test_rejects_empty_course_name(self):
+        with self.assertRaises(TypeError, msg='Course name should not be blank.'):
+            CourseAPI.create_course(self.course_code1, '', self.user1)
 
     def test_get_course_by_course_code(self):
         course2 = CourseAPI.get_course_by_course_code(self.course_code1)
         self.assertEqual(self.course1, course2, msg="Expected the course to be retrieved by its course code.")
 
     def test_get_course_by_course_code_not_in_database(self):
-        response = CourseAPI.get_course_by_course_code(self.course_code2)
+        course_code = '361'
+        response = CourseAPI.get_course_by_course_code(self.course_code3)
         self.assertEqual(None, response, msg="Expected None when course does not exist in database.")
 
     def test_get_course_by_course_id(self):
         course_id = self.course1.course_id
         course = CourseAPI.get_course_by_course_id(course_id)
         self.assertEqual(course, self.course1, msg="Expected course1 when using its course id to retrieve.")
+
+    def test_get_all_courses(self):
+        course_set = CourseAPI.get_all_courses()
+        list_of_courses = list(course_set)
+        self.assertEqual(2, list_of_courses.__len__(), msg='Expected list of courses to be 2.')
+        self.assertTrue(self.course1 in list_of_courses, msg='Exepect course1 in all courses.')
+        self.assertTrue(self.course2 in list_of_courses, msg='Expected course2 in all courses.')
 
     def test_delete_course(self):
         CourseAPI.delete_course(self.course1)
