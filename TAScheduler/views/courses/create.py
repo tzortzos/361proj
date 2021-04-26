f79966f3b9088a2d613ecd0fc0e88007e5b42c44
Josiah Hilden <jhilden13@gmail.com>
2021-04-26 17:36:05 -0500

    Finish course create view

--- TAScheduler/views/courses/create.py
+++ TAScheduler/views/courses/create.py
@@ -4,10 +4,11 @@ from django.shortcuts import render, redirect, reverse
 from typing import List, Union, Optional
 from string import digits
 
-from TAScheduler.ClassDesign.LoginUtility import LoginUtility
-from TAScheduler.ClassDesign.UserAPI import UserType
+from TAScheduler.ClassDesign.LoginUtility import LoginUtility, UserType
+from TAScheduler.ClassDesign.CourseAPI import CourseAPI
 from TAScheduler.viewsupport.message import MessageQueue, Message
 from TAScheduler.viewsupport.navbar import AdminItems
+from TAScheduler.viewsupport.errors import CourseError
 
 class CoursesCreate(View):
 
@@ -48,15 +49,30 @@ class CoursesCreate(View):
         course_code: Optional[str] = request.POST.get('course_code', None)
         course_name: Optional[str] = request.POST.get('course_name', None)
 
-        if course_code is None or (a not in set(digits) for a in course_code):
+        if course_code is None or (a not in set(digits) for a in course_code) or len(course_code) != 3:
             return render(request, 'pages/courses/edit_create.html', {
                 'self': user,
                 'navbar_items': AdminItems.items_iterable(),
-                'messages': MessageQueue.drain(),
+                'messages': MessageQueue.drain(request.session),
 
-                # TODO add error
+                'error': CourseError(
+                    'A course code must be exactly 3 digits',
+                    CourseError.Place.CODE
+                ),
             })
 
-        # TODO check name
+        if course_name is None:
+            return render(request, 'pages/courses/edit_create.html', {
+                'self': user,
+                'navbar_items': AdminItems.items_iterable(),
+                'messages': MessageQueue.drain(request.session),
 
+                'error': CourseError(
+                    'A course code must be exactly 3 digits',
+                    CourseError.Place.NAME,
+                ),
+            })
 
+        course: int = CourseAPI.create_course(course_code, course_name, user)
+
+        return redirect(reverse('courses-view', args=[course]))
