85c4855c4b10db477cdd28d1cbf7724027099ad0
Josiah Hilden <jhilden13@gmail.com>
2021-04-29 14:53:32 -0500

    Make admin views better for many to many fields

--- TAScheduler/admin.py
+++ TAScheduler/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from TAScheduler.models import *
 
+class RecipientInline(admin.TabularInline):
+    model = Recipient
+    exclude = ('read',)
+    extra = 0
+
+class MessageAdmin(admin.ModelAdmin):
+    inlines = (RecipientInline,)
+
+class AssignmentInline(admin.TabularInline):
+    model = Assignment
+    extra = 0
+
+class SectionAdmin(admin.ModelAdmin):
+    inlines = (AssignmentInline,)
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Course)
-admin.site.register(Section)
+admin.site.register(Section, SectionAdmin)
 admin.site.register(Lab)
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
 admin.site.register(Skill)
+admin.site.register(Recipient)
