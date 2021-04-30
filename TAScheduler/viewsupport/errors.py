a95d27e4673cf96e54b2947525393faf11072537
Josiah Hilden <jhilden13@gmail.com>
2021-04-30 08:40:27 -0500

    Login Page errors reworked and class cleaned up

--- TAScheduler/viewsupport/errors.py
+++ TAScheduler/viewsupport/errors.py
@@ -1,77 +1,47 @@
-from typing import Optional, Union
+from __future__ import annotations
+from typing import Optional, Union, Generic, TypeVar, Type
 from enum import Enum, auto
 
-class PageError:
+P = TypeVar('P', bound=Enum)
+
+
+class PageError(Generic[P]):
     """
     Represents an error which may or may not have a headline, and which should be rendered with the
     `partials/inline_error.html` partial.
     """
 
-    def __init__(self, body: str, headline: Optional[str] = None):
+    def __init__(self, msg: str, place: P):
         """
         Create a new basic error with or without a headline value
         """
-        self._body = body
-        self._headline = headline
-
-    def has_headline(self) -> bool:
-        """
-        Returns true if the error has a headline value, used by template
-        to render as a error panel instead of alert.
-        :return:
-        """
-        return self._headline is not None
-
-    def headline(self) -> str:
-        """
-        Raises TypeError in the case that this error has no headline,
-        use has_headline() to check beforehand.
-        """
-
-        if self._headline is None:
-            raise TypeError('Tried to get headline from error without one')
-
-        return self._headline
-
-    def body(self) -> str:
-        """
-        Returns the body text of this error
-        :return:
-        """
-
-        return self._body
-
-
-class LoginError:
-    """
-    Represents an error to be used on the login page,
-    placed with the username or password field depending on the variant
-    """
-
-    class Place(Enum):
-        USERNAME = 0
-        PASSWORD = 1
-
-    def __init__(self, error_text: Union[PageError, str], place: Place):
+        self._msg = msg
         self._place = place
-        if type(error_text) is PageError:
-            self._error = error_text
-        elif type(error_text) is str:
-            self._error = PageError(error_text)
-        else:
-            raise TypeError('LoginError must be of type PageError or str')
 
-    def error(self) -> PageError:
-        """Gets the inner error of this instance of LoginError"""
-        return self._error
+    def message(self) -> str:
+        """Return the message contained in this error"""
+        return self._msg
 
-    def place_username(self):
-        """Returns true iff this login error is associated with the login field"""
-        return self._place == LoginError.Place.USERNAME
+    def place(self) -> P:
+        """Return the placement of this error"""
+        return self._place
 
-    def place_password(self):
-        """Returns true iff this login error is associated with the password field"""
-        return self._place == LoginError.Place.PASSWORD
+
+
+class LoginPlace(Enum):
+    USERNAME = 0
+    PASSWORD = 1
+
+    def username(self) -> bool:
+        return self is LoginPlace.USERNAME
+
+    def password(self) -> bool:
+        return self is LoginPlace.PASSWORD
+
+
+# Export a Concertized variant of PageError for consumption
+"""Represents errors that can be displayed on the login page"""
+LoginError = PageError[LoginPlace]
 
 
 class UserEditError:
