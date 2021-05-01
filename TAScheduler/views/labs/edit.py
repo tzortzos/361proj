773741f28bcfc056a229cdaad91b99344788f08c
Josiah Hilden <jhilden13@gmail.com>
2021-05-01 15:40:10 -0500

    cleaned labs-edit

--- TAScheduler/views/labs/edit.py
+++ TAScheduler/views/labs/edit.py
@@ -77,7 +77,7 @@ class LabsEdit(View):
         lab_day = request.POST.get('lab_day', '')
         lab_time = request.POST.get('lab_time', '')
 
-        if section_id is None:
+        def render_error(error: LabEditError):
             return render(request, 'pages/labs/edit_create.html', {
                 'self': user,
                 'navbar_items': AdminItems.items_iterable(),
@@ -86,38 +86,23 @@ class LabsEdit(View):
                 'sections': Section.objects.all(),
                 'tas': User.objects.filter(type=UserType.TA),
 
-                'error': LabEditError('You cannot remove the 3 digit lab code', LabEditPlace.SECTION),
+                'error': error,
             })
+
+        if section_id is None:
+            return render_error(LabEditError('You cannot remove the 3 digit lab code', LabEditPlace.SECTION))
 
         non_digits = ilen((a for a in lab_code if a not in set(digits)))
 
         if lab_code is None or len(lab_code) != 3 or non_digits > 0:
-            return render(request, 'pages/labs/edit_create.html', {
-                'self': user,
-                'navbar_items': AdminItems.items_iterable(),
-                'messages': MessageQueue.drain(request.session),
-
-                'sections': Section.objects.all(),
-                'tas': User.objects.filter(type=UserType.TA),
-
-                'error': LabEditError('You cannot remove the 3 digit lab code', LabEditPlace.CODE),
-            })
+            return render_error(LabEditError('You cannot remove the 3 digit lab code', LabEditPlace.CODE))
 
         lab.code = lab_code
 
         section = SectionAPI.get_by_id(section_id)
 
         if section is None:
-            return render(request, 'pages/labs/edit_create.html', {
-                'self': user,
-                'navbar_items': AdminItems.items_iterable(),
-                'messages': MessageQueue.drain(request.session),
-
-                'sections': Section.objects.all(),
-                'tas': User.objects.filter(type=UserType.TA),
-
-                'error': LabEditError('You cannot remove a section from this lab', LabEditPlace.SECTION),
-            })
+            return render_error(LabEditError('You cannot remove a section from this lab', LabEditPlace.SECTION))
 
         lab.section = section
 
