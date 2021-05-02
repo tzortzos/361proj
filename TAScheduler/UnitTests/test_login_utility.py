b70bfc5621452125fc9cc8778f35c78e2523511a
NRL <lnahnan@uwm.edu>
2021-05-02 15:55:59 -0500

    LoginUtility renames Still having issues with get/validate user method

--- TAScheduler/UnitTests/test_login_utility.py
+++ TAScheduler/UnitTests/test_login_utility.py
@@ -7,13 +7,21 @@ from TAScheduler.models import User
 class TestLoginUtility(TestCase):
 
     def setUp(self) -> None:
-        self.user1 = User.objects.create(type='A',univ_id=str(uuid.uuid4())[:6], password=str(uuid.uuid4())[:8])
+
+        self.username1 = str(uuid.uuid4())[:6]
+        self.password1 = str(uuid.uuid4())[:8]
+        self.user1 = User.objects.create(type='A',username=self.username1, password=self.password1)
 
     def test_update_password(self):
         new_pass = str(uuid.uuid4())[:8]
         LoginUtility.update_password(self.user1,new_pass)
         self.assertEqual(new_pass, self.user1.password, msg='Expected the password to have been updated.')
         self.assertEqual(False, self.user1.password_tmp, msg='Expected tmp password boolean to change to False.')
+
+    def test_generate_tmp_password(self):
+        tmp_pass = LoginUtility.generate_tmp_password()
+        self.assertTrue(tmp_pass is not None, msg="Expected password to be generated.")
+        self.assertTrue(len(tmp_pass) == 8, msg='Expected tmp passowrd to the be 8 characters.')
 
     def test_get_user_and_validate_by_user_id(self):
         new_pass = str(uuid.uuid4())[:8]
