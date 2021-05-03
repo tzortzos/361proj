1756563710c7e17cd62238a5c817f6239e44f8d3
NRL <lnahnan@uwm.edu>
2021-05-02 19:32:39 -0500

    LoginUtility update with client session slight modification to unit tests (passing)

--- TAScheduler/UnitTests/test_login_utility.py
+++ TAScheduler/UnitTests/test_login_utility.py
@@ -1,6 +1,7 @@
 import uuid
 from TAScheduler.ClassDesign.LoginUtility import LoginUtility
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.shortcuts import reverse
 from TAScheduler.models import User
 
 
@@ -8,9 +9,17 @@ class TestLoginUtility(TestCase):
 
     def setUp(self) -> None:
 
+        self.client = Client()
+        self.session = self.client.session
+
+
         self.username1 = str(uuid.uuid4())[:6]
         self.password1 = str(uuid.uuid4())[:8]
         self.user1 = User.objects.create(type='A',username=self.username1, password=self.password1)
+
+        self.session['user_id'] = self.user1.id
+        self.session.save()
+
 
     def test_update_password(self):
         new_pass = str(uuid.uuid4())[:8]
@@ -26,13 +35,15 @@ class TestLoginUtility(TestCase):
     def test_get_user_and_validate_by_user_id(self):
         new_pass = str(uuid.uuid4())[:8]
         LoginUtility.update_password(self.user1, new_pass)
-        user = LoginUtility.get_user_and_validate_by_user_id(self.user1.id)
+        user = LoginUtility.get_user_and_validate_by_user_id(self.session)
         self.assertEqual(self.user1, user, msg='Expected user to be returned because exists and tmp password false')
 
     def test_get_user_and_validate_by_user_with_edit_redirect(self):
-        user = LoginUtility.get_user_and_validate_by_user_id(self.user1.id)
-        self.assertEqual('/user-edit/', user.url, msg='Expected redirect to password updated page.')
+        user = LoginUtility.get_user_and_validate_by_user_id(self.session)
+        self.assertEqual(reverse('users-edit', args=[self.user1.id]), user.url, msg='Expected redirect to password updated page.')
 
     def test_get_user_and_validate_by_user_login_redirect(self):
-        user = LoginUtility.get_user_and_validate_by_user_id(0)
+        del self.session['user_id']
+        self.session.save()
+        user = LoginUtility.get_user_and_validate_by_user_id(self.session)
         self.assertEqual('/login/', user.url, msg='Expected redirect to login for non-existent user.')
