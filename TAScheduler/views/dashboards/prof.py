d1a5898860422a094b65fdf1dfe7f1873a92113b
chris <cmwojta@uwm.edu>
2021-05-11 12:16:42 -0500

    added to code in prof dashboard

---
+++ TAScheduler/views/dashboards/prof.py
@@ -0,0 +1,20 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.views import View
+from django.shortcuts import render, redirect, reverse
+from typing import List, Union
+
+from TAScheduler.ClassDesign.LoginUtility import LoginUtility
+from TAScheduler.viewsupport.message import MessageQueue, Message
+from TAScheduler.viewsupport.navbar import AdminItems
+from TAScheduler.ClassDesign.CourseAPI import Course, CourseAPI
+from TAScheduler.ClassDesign.UserAPI import User
+
+def get(request: HttpRequest, user: User) -> HttpResponse:
+    return render(request, 'pages/dashboards/prof.html', {
+        "self": user,
+        "messages": MessageQueue.drain(request.session),
+        "navbar_items": AdminItems.HOME.items_iterable_except(),
+
+        "message_count": 17,
+        "courses": Course.objects.all()
+    })
